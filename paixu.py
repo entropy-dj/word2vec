5430e3371f8dda15a3acc29fd425c19f4c5050fe
entropy <entropy_dj@163.com>
2020-04-23 11:13:00 +0800

    测试2020-4-23 11:12:53

--- paixu.py
+++ paixu.py
@@ -1,7 +1,15 @@
 a = [1, 4, 3, 5, 2]
 b = ['c', 'a', 'd', 'b', 'e']
 """
-变成字典
+第一种排序，变成list，里面全是元组
 """
-data = dict(zip(a, b))
-print(data)
+data1 = [(x, y) for x, y in zip(a, b)]
+# 按照第一个排序
+result = sorted(data1, key= lambda x:x[0])
+print(result)
+"""
+第二种排序，用zip
+"""
+data2 = zip(a, b)
+result = sorted(data2)  # 看zip第一个元素是谁(当前是a)
+print(result)
